da745eb939b028ccd7e0553f41fc2b232a84ca9b
Thibaut-Renaut <renautthibaut4@gmail.com>
2023-12-11 13:17:15 +0100

    Version 1.1 ihm (onglets)

---
+++ code/bin/back/mld.sql
@@ -0,0 +1,71 @@
+DROP TABLE IF EXISTS Heure       CASCADE;
+DROP TABLE IF EXISTS Type_Heure  CASCADE;
+DROP TABLE IF EXISTS Intervenant CASCADE;
+DROP TABLE IF EXISTS Statut      CASCADE;
+DROP TABLE IF EXISTS Module      CASCADE;
+
+Create Table Module (
+    id_module serial primary key,
+    nom_module VARCHAR(50),
+    nb_heure integer NOT NULL
+);
+/*
+Un module possèdera :
+1. Un identifiant (un nombre entier positif).
+2. Un nom (de 50 caractères maximum).
+3. Un nombre d’heure associé
+*/
+
+
+Create table Type_Heure(
+    id_type_heure serial primary key,
+    coeff float
+);
+/*
+Un type d’heure possèdera :
+1. Un identifiant (de 15 caractères maximum)
+2. Un coefficient (un nombre décimal)
+*/
+
+
+Create table Statut (
+    id_statut VARCHAR(10) primary key,
+    nbHeureMini float check(nbHeureMini>0),
+    nbHeureMax  float check(nbHeureMax>0)
+);
+/*
+Un statut possèdera :
+1. Un identifiant (de 10 caractères maximum)
+2. Un nombre d’heures minimum
+3. Un nombre d’heures maximum
+*/
+
+
+Create table Intervenant (
+    id_intervenant serial primary key,
+    nom VARCHAR(25),
+    id_statut VARCHAR(10) REFERENCES Statut(id_statut)
+);
+/*
+Un intervenant possèdera :
+1. Un identifiant (un nombre entier positif)
+2. Un nom (de 25 caractères maximum)
+3. Un statut associé
+*/
+
+
+Create table Heure (
+    id_heure serial primary key,
+    id_module integer REFERENCES Module(id_module),
+    id_intervenant integer REFERENCES Intervenant(id_intervenant),
+    id_type_heure integer REFERENCES Type_Heure(id_type_heure),
+    nb_heures integer NOT NULL CHECK(nb_heures > 0)
+);
+/*
+Une heure possèdera :
+1. Un identifiant (un nombre entier positif)
+2. Un module
+3. Un intervenant
+4. Un type d’heure
+5. Un nombre d’heures (un nombre entier positif)
+*/
--- code/src/ihm/Panels/PanelCenterPrevi.java
+++ code/src/ihm/Panels/PanelCenterPrevi.java
@@ -1,14 +1,84 @@
 package ihm.Panels;
 
+import java.awt.*;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 import javax.swing.JTextField;
 
 public class PanelCenterPrevi extends JPanel {
-	private JTextField textFieldTD;
-	private JTextField textFieldTP;
+	private JTextField textFieldGpTD;
+	private JTextField textFieldGpTP;
+	private JTextField textFieldNbEt;
+	private JTextField textFieldNbSemaine;
 	
 
 	public PanelCenterPrevi() {
-		//jtabbedPane = new JTabbedPane(JTabbedPane.TOP);
+        setSize(400, 300);
+
+        JTabbedPane modules = new JTabbedPane();
+
+        // Créer des panneaux pour chaque onglet
+        JPanel semestre1 = new JPanel();
+        semestre1.add(new JLabel("Contenu de l'onglet 1"));
+        
+        JPanel semestre2 = new JPanel();
+        semestre2.add(new JLabel("Contenu de l'onglet 2"));
+
+        JPanel semestre3 = new JPanel();
+        semestre3.add(new JLabel("Contenu de l'onglet 3"));
+
+		JPanel semestre4 = new JPanel();
+		semestre4.add(new JLabel("Contenu de l'onglet 4"));
+
+		JPanel semestre5 = new JPanel();
+		semestre5.add(new JLabel("Contenu de l'onglet 5"));
+
+		JPanel semestre6 = new JPanel();
+		semestre6.add(new JLabel("Contenu de l'onglet 6"));
+
+        // Ajouter les onglets au JTabbedPane
+        modules.addTab("Onglet 1", semestre1);
+        modules.addTab("Onglet 2", semestre2);
+        modules.addTab("Onglet 3", semestre3);
+		modules.addTab("Onglet 4", semestre4);
+		modules.addTab("Onglet 5", semestre5);
+		modules.addTab("Onglet 6", semestre6);
+
+        // Ajouter le JTabbedPane à la fenêtre
+        add(modules);
+
+        setVisible(true);
+	}
+
+	public JPanel remplissagPanel(JPanel global){
+		//Layout
+		global.setLayout(new BorderLayout());
+
+		//JPanels
+		JPanel panelNord = new JPanel();
+		JPanel panelCentre = new JPanel();
+
+		panelNord.add(new JLabel("nb gp TD"));
+		textFieldGpTD = new JTextField(2);
+		panelNord.add(textFieldGpTD);
+
+		panelNord.add(new JLabel("nb gp TP"));
+		textFieldGpTP = new JTextField(2);
+		panelNord.add(textFieldGpTP);
+
+		panelNord.add(new JLabel("nb Etd"));
+		textFieldNbEt = new JTextField(2);
+		panelNord.add(textFieldNbEt);
+
+		panelNord.add(new JLabel("nb semaines"));
+		textFieldNbSemaine = new JTextField(2);
+		panelNord.add(textFieldNbSemaine);
+
+		global.add(panelNord, BorderLayout.NORTH);
+		
+		return global;
 	}
 }
