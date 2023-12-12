2116ca3b4a09337c466149429995e11207f2412f
Biyiv <baptiste.hay@hotmail.fr>
2023-12-12 11:31:03 +0100

    Version 1.3.4 ihm
    Agencement PRNord

--- src/ihm/previsionnel/ressources/PanelRessources.java
+++ src/ihm/previsionnel/ressources/PanelRessources.java
@@ -1,20 +1,27 @@
 package ihm.previsionnel.ressources;
-//Imports classes Java
-import javax.swing.JPanel;
 
-//Imports des classes externes
-import ihm.previsionnel.*;
+import ihm.previsionnel.PanelPrevi;
+import javax.swing.*;
+import java.awt.*;
+
 import ihm.previsionnel.ressources.ressourcesNord.PRNord;
 
-public class PanelRessources extends JPanel{
-	//Attributs
-	private PanelPrevi frame;
-	private PRNord pRNord;
+public class PanelRessources extends JPanel {
+    private PanelPrevi frame;
+    private JPanel panelPrincipalNord;
+    private PRNord pRNord;
 
-	public PanelRessources(PanelPrevi frame){
-		//Synchronisation des pages
-		this.frame = frame;
+    public PanelRessources(PanelPrevi frame) {
+        this.frame = frame;
+        this.setLayout(new BorderLayout());
 
-		this.pRNord = new PRNord(this);
-	}
+        this.panelPrincipalNord = new JPanel();
+        this.pRNord = new PRNord(this);
+
+        // Utiliser BoxLayout pour organiser les composants horizontalement
+        this.panelPrincipalNord.setLayout(new BoxLayout(this.panelPrincipalNord, BoxLayout.X_AXIS));
+        this.panelPrincipalNord.add(this.pRNord);
+        this.panelPrincipalNord.add(Box.createHorizontalGlue()); // Ajouter de l'espace horizontal flexible
+        this.add(this.panelPrincipalNord, BorderLayout.NORTH);
+    }
 }
--- src/ihm/previsionnel/ressources/ressourcesNord/PRNord.java
+++ src/ihm/previsionnel/ressources/ressourcesNord/PRNord.java
@@ -1,32 +1,108 @@
 package ihm.previsionnel.ressources.ressourcesNord;
 
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
-//Import classes java
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import ihm.previsionnel.ressources.PanelRessources;
 
 public class PRNord extends JPanel {
 	private PanelRessources 	panelMere		;
-	private PanelModule 		panelModule		;
-	private PanelHeureModule 	panelHeureModule;
+	//contenu
+	private JPanel panelCentre	;
 
+	//pnlModule
+	private JTextField 	txtTypeModule	;
+	private JTextField 	txtSemestre		;
+	private JTextField 	txtCode			;
+	private JTextField 	txtLibLong		;
+	private JTextField 	txtLibCourt		;
+
+	//pnlHeureModule
+	private JTextField txtNbEtd;
+    private JTextField txtNbGpTd;
+    private JTextField txtNbGpTp;
+	
 	public PRNord(PanelRessources panelMere){
 		//Synchronisation des pages
 		this.panelMere = panelMere;
 
 		this.setLayout(new BorderLayout());
 
-		//Initialisation des panels
-		this.panelModule		 = new PanelModule		(this);
-		this.panelHeureModule	 = new PanelHeureModule	(this);
+		//Initialisation des composants
+		this.panelCentre 	= new JPanel();
+		this.txtTypeModule	= new JTextField("a completer", 10);
+		this.txtSemestre	= new JTextField("a completer", 10);
+		this.txtCode 		= new JTextField(10);
+		this.txtLibLong 	= new JTextField(10);
+		this.txtLibCourt 	= new JTextField(10);
+		this.txtNbEtd		= new JTextField(2);
+		this.txtNbGpTd		= new JTextField(2);
+		this.txtNbGpTp		= new JTextField(2);
 
 		//Initialisation d'un panel de mise en page
 		JPanel pnlEstVide		 = new JPanel				();
+		this.panelCentre.setLayout(new GridBagLayout());
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		// Ajout des composants avec GridBagLayout
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.insets = new Insets(0, 0, 5, 5); // Marge entre les composants
+		
+		// Ajouts des éléments
+		this.panelCentre.add(new JLabel("type module"		), gbc);
+		gbc.gridx = 1;
+		this.panelCentre.add(new JLabel("semestre"			), gbc);
+		gbc.gridx = 2;
+		this.panelCentre.add(new JLabel("code"				), gbc);
+		gbc.gridx = 3;
+		this.panelCentre.add(new JLabel("libellé long"		), gbc	);
+		gbc.gridx = 4;
+		this.panelCentre.add(new JLabel("libellé court"	), gbc	);
+		gbc.gridx = 0;
+		gbc.gridy = 1;
+		this.panelCentre.add(this.txtTypeModule, gbc					);
+		gbc.gridx = 1;
+		this.panelCentre.add(this.txtSemestre, gbc				);
+		gbc.gridx = 2;
+		this.panelCentre.add(this.txtCode, gbc						);
+		gbc.gridx = 3;
+		gbc.insets = new Insets(0, 0, 5, 10); // Marge entre les composants
+		this.panelCentre.add(this.txtLibLong, gbc					);
+		gbc.gridx = 4;
+		this.panelCentre.add(this.txtLibCourt, gbc					);
+		//Pour empêcher la modification des champs de saisies 
+		this.txtTypeModule	.setEditable(false);
+		this.txtSemestre	.setEditable(false);
 
-		this.add(this.panelModule, BorderLayout.CENTER		);
-		this.add(this.panelHeureModule, BorderLayout.SOUTH	);
+		
+		//pnlHeureModule
+		// Ajout des composants avec GridBagLayout
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.insets = new Insets(0, 10, 5, 0); // Marge entre les composants
+		this.panelCentre.add(new JLabel("nb Etd"), gbc);
+		gbc.gridx = 1;
+		this.panelCentre.add(new JLabel("nb gp TD"), gbc);
+		gbc.gridx = 2;
+		this.panelCentre.add(new JLabel("nb gp TP"), gbc);
+
+		gbc.gridx = 0;
+        gbc.gridy = 3;
+		this.panelCentre.add(this.txtNbEtd, gbc);
+		gbc.gridx = 1;
+		this.panelCentre.add(this.txtNbGpTd, gbc);
+		gbc.gridx = 2;
+		this.panelCentre.add(this.txtNbGpTp, gbc);
+
+
+		this.add(this.panelCentre, BorderLayout.WEST);;
+
+		this.setVisible(true);
 		
 	}
 }
