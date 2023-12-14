c44080dd646e9da1d133477e442ce126cc201b05
Florian Janot <janotflorian@gmail.com>
2023-12-14 10:27:26 +0100

    controleurMetier

--- src/metier/ControleurMetier.java
+++ src/metier/ControleurMetier.java
@@ -2,16 +2,22 @@ package metier;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
 
 import metier.db.*;
 
 public class ControleurMetier {
 
     private Requetes requetes;
+    private List<Statut>    statuts;
+    private List<TypeHeure> typesHeures;
 
 
     public ControleurMetier() {
         this.requetes = new Requetes();
+
+        this.statuts     = new ArrayList<Statut>();
+        this.typesHeures = new ArrayList<TypeHeure>();
     }
 
 
@@ -19,7 +25,4 @@ public class ControleurMetier {
         this.requetes.insertIntervenant(intervenant);
         return false;
     }
-
-
-
 }
